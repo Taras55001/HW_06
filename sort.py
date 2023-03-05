981b1db3c1929bc784fafe33e406006270f7ae0b
Taras55001 <taras5500@gmail.com>
2023-03-05 17:38:03 +0100

    Update sort.py
    add lists_of_result function

--- sort.py
+++ sort.py
@@ -22,6 +22,51 @@ def creating_folders(destination):
         destination.mkdir()
 
 
+def lists_of_result(p):
+    list_of_all = []
+    Image_list = []
+    video_list = []
+    music_list = []
+    unknown_list = []
+
+    for i in p.iterdir():
+        if i.name == 'Images':
+            for j in i.iterdir():
+                if j.name.split('.')[-1] not in list_of_all:
+                    list_of_all.append(j.name.split('.')[-1])
+                Image_list.append(j.name)
+        if i.name == 'Videos':
+            for j in i.iterdir():
+                if j.name.split('.')[-1] not in list_of_all:
+                    list_of_all.append(j.name.split('.')[-1])
+                video_list.append(j.name)
+        if i.name == 'Documents':
+            for j in i.iterdir():
+                if j.name.split('.')[-1] not in list_of_all:
+                    list_of_all.append(j.name.split('.')[-1])
+                Image_list.append(j.name)
+        if i.name == 'Music':
+            for j in i.iterdir():
+                if j.name.split('.')[-1] not in list_of_all:
+                    list_of_all.append(j.name.split('.')[-1])
+                music_list.append(j.name)
+        if i.name == 'Unknown':
+            for j in i.iterdir():
+                if j.name.split('.')[-1] not in list_of_all:
+                    list_of_all.append(j.name.split('.')[-1])
+                unknown_list.append(j.name)
+    if list_of_all:
+        print(f'List of extension: {list_of_all}')
+    if Image_list:
+        print(f'Files in Images: {Image_list}')
+    if video_list:
+        print(f'Files in Videos: {video_list}')
+    if music_list:
+        print(f'Files in Music: {music_list}')
+    if unknown_list:
+        print(f'Files in Unknown: {unknown_list}')
+
+
 def moving_file(destination, item):
     creating_folders(destination)
     try:
@@ -34,11 +79,11 @@ def normalize(p):
     # функція для перейменування файлів
     for item in p.iterdir():
         if item.is_file():
-
             a = (item.name.split('.')[0]).translate(TRANS)
             b = item.name.split('.')[-1]
-            new_filename = p / (a + '.' + b)
-            Path.rename(item, new_filename)
+            new_filename = a + '.' + b
+            new_file_path = p / new_filename
+            Path.rename(item, new_file_path)
         elif item.is_dir():
             normalize(item)
 
@@ -63,7 +108,6 @@ def unpuckarhives(p):
 def sort_folder(path):
     p = Path(path)
     unpuckarhives(p)
-
     for item in p.iterdir():
         global s
         if item.is_file():
@@ -98,6 +142,7 @@ def sort_folder(path):
         if folder.is_dir() and not any(folder.iterdir()):
             folder.rmdir()
     normalize(p)
+    lists_of_result(p)
 
 
 if __name__ == '__main__':
